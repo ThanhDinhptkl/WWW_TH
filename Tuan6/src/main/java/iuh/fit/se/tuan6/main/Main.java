78635eb58070f9a2bf3018c8bf56a4a72048295b
H82M19\Student <Student>
2025-10-12 08:18:21 +0700

    Bài 2 Dùng Annotation-Based Configuration @bean

--- Tuan6/src/main/java/iuh/fit/se/tuan6/main/Main.java
+++ Tuan6/src/main/java/iuh/fit/se/tuan6/main/Main.java
@@ -1,17 +1,38 @@
+
+//Dùng XML-Based Configuration
+//package iuh.fit.se.tuan6.main;
+//
+//import iuh.fit.se.tuan6.entities.Employee;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//public class Main {
+//    private static ApplicationContext context;
+//
+//    public static void main(String[] args) {
+//        context = new ClassPathXmlApplicationContext("beans.xml");
+//        Employee employee1 = context.getBean("employee", Employee.class);
+//        Employee employee2 = context.getBean("employee2", Employee.class);
+//        System.out.println("Employee 1: " + employee1);
+//        System.out.println("Employee 2: " + employee2);
+//    }
+//}
+
+
+// Sử dụng @Bean trong class Config thay vì file XML
 package iuh.fit.se.tuan6.main;
 
 import iuh.fit.se.tuan6.entities.Employee;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import iuh.fit.se.tuan6.config.Config;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    private static ApplicationContext context;
-
     public static void main(String[] args) {
-        context = new ClassPathXmlApplicationContext("beans.xml");
-        Employee employee1 = context.getBean("employee", Employee.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Employee employee1 = context.getBean("employee1", Employee.class);
         Employee employee2 = context.getBean("employee2", Employee.class);
         System.out.println("Employee 1: " + employee1);
         System.out.println("Employee 2: " + employee2);
+        context.close();
     }
 }
